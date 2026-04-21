349e1deff97bed8191811773edd191a564443e4b
Samuel Martins Francisco <samuelmartinsfrancisco@gmail.com>
2026-04-20 23:47:37 -0300

    fix: remove wrong encryption on seed

--- src/infrastructure/database/seeds/seeds.module.ts
+++ src/infrastructure/database/seeds/seeds.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { SeedsService } from './seeds.service';
-import { CryptoModule } from '@/utils/crypto/crypto.module';
 import { ConfigModule } from '@nestjs/config';
 import { CredentialsModule } from '@/modules/auth/credentials/credentials.module';
 import { UserModule } from '@/modules/users/user.module';
@@ -8,7 +7,6 @@ import { DatabaseModule } from '../database.module';
 
 @Module({
   imports: [
-    CryptoModule,
     ConfigModule.forRoot({
       expandVariables: true,
     }),
--- src/infrastructure/database/seeds/seeds.service.ts
+++ src/infrastructure/database/seeds/seeds.service.ts
@@ -3,8 +3,6 @@ import { UserRepository } from "@/modules/users/user.repository";
 import { CredentialsRepository } from "@/modules/auth/credentials/credentials.repository";
 import { ConfigService } from "@nestjs/config";
 import { RegisterDTOWithoutId } from "@/modules/auth/auth.dto";
-import { CryptoService } from "@/utils/crypto/crypto.service";
-import * as argon2 from 'argon2';
 import { UserRole } from "@/modules/users/user.dto";
 
 @Injectable()
@@ -13,7 +11,6 @@ export class SeedsService {
     private readonly userRepository: UserRepository,
     private readonly credentialsRepository: CredentialsRepository,
     private readonly configService: ConfigService,
-    private readonly cryptoService: CryptoService
   ) { }
 
   async seed() {
@@ -24,20 +21,18 @@ export class SeedsService {
     const email = this.configService.get<string>('ADMIN_EMAIL');
     const password = this.configService.get<string>('ADMIN_PASSWORD');
     const name = this.configService.get<string>('ADMIN_NAME') || 'Admin';
-    const lastName = this.configService.get<string>('ADMIN_LAST_NAME') || 'User';
+    const lastName = this.configService.get<string>('ADMIN_LASTNAME') || 'User';
 
     if (!email || !password) {
       throw new InternalServerErrorException('ADMIN_EMAIL and ADMIN_PASSWORD environmet variable must be set');
     }
 
-    const passwordHash = await argon2.hash(password);
-
     const adminData: RegisterDTOWithoutId = {
-      name: this.cryptoService.encrypt(name),
-      lastName: this.cryptoService.encrypt(lastName),
+      name,
+      lastName,
       role: UserRole.ADMIN,
-      email: this.cryptoService.encrypt(email),
-      password: passwordHash
+      email,
+      password,
     }
 
     return adminData;
